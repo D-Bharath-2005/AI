b5ff0e0fdb6212175c5b7b2f178da299937d68fd
BHARATH <129199194+D-Bharath-2005@users.noreply.github.com>
2024-11-03 19:11:12 +0530

    Update Towers of Hanoi.pl

--- Towers of Hanoi.pl
+++ Towers of Hanoi.pl
@@ -5,3 +5,5 @@ move(N, Source, Target, Auxiliary) :-
     move(N1, Source, Auxiliary, Target),
     write('Move disk from '), write(Source), write(' to '), write(Target), nl,
     move(N1, Auxiliary, Target, Source).
+
+% move(3, a, c, b).
